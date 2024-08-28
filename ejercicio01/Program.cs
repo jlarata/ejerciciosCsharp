2dd6ec82814309849182753b40e59fea554fcbe1
jlarata <ariel@cfee.gob.ar>
2024-08-28 12:09:19 -0300

    ...

---
+++ ejercicio01/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+Persona pepe = new Persona{};
+pepe.nombre = "pepe";
+
+Console.WriteLine($"bienvenido, {pepe.nombre}");
+
+class Persona {
+    public string nombre = "iniciado sin nombre";
+}
