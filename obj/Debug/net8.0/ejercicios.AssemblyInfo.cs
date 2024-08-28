ae8f2ad8b4faba633f6520cd4418cfdc1fba0f1a
jlarata <ariel@cfee.gob.ar>
2024-08-28 12:12:14 -0300

    ...

--- obj/Debug/net8.0/ejercicios.AssemblyInfo.cs
+++ obj/Debug/net8.0/ejercicios.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ejercicios")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+26823622ae91b4fea51390c3663fa03001c5fba1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2dd6ec82814309849182753b40e59fea554fcbe1")]
 [assembly: System.Reflection.AssemblyProductAttribute("ejercicios")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ejercicios")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
