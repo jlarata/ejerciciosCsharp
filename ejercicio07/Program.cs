8de2fb1668d653e1c27960cf40ef99245e2f175a
jlarata <ariel@cfee.gob.ar>
2024-08-29 16:30:56 -0300

    ...

---
+++ ejercicio07/Program.cs
@@ -0,0 +1,59 @@
+﻿/* Interfaces
+Last updated: 8/26/2020 ⁃ Difficulty: Intermediate
+
+Create a C# program that implements an IVehiculo interface with two methods, one for Drive of type void
+ and another for Refuel of type bool that has a parameter of type integer with the amount of gasoline
+  to refuel. Then create a Car class with a builder that receives a parameter with the car's starting
+   gasoline amount and implements the Drive and Refuel methods of the car.
+
+The Drive method will print on the screen that the car is Driving, if the gasoline is greater than 0.
+ The Refuel method will increase the gasoline of the car and return true.
+
+To carry out the tests, create an object of type Car with 0 of gasoline in the Main of the program 
+and ask the user for an amount of gasoline to refuel, finally execute the Drive method of the car.
+Input
+
+    50
+
+Output
+
+    Driving*/
+
+class Interfaces{
+
+    static void Main() {
+
+        IVehiculo autito = new Car(0);
+        autito.Drive();
+        autito.Refuel(1);
+        autito.Drive();
+    }
+
+    interface IVehiculo {
+        void Drive() {
+
+        }
+
+        bool Refuel(int gasoline) {
+            return true;
+        }
+    }
+
+    class Car(int startingGas) : IVehiculo {
+        public int gasoline = startingGas;
+
+        void IVehiculo.Drive() {
+            if (gasoline > 0) {
+                Console.WriteLine("driving");
+            } else {
+                Console.WriteLine("not enough gas...");
+            }
+        }
+
+        bool IVehiculo.Refuel(int gasCharge) {
+            gasoline += gasCharge;
+            return true;
+        }
+
+    }
+}
---
+++ ejercicioPublicidades/Program.cs
@@ -0,0 +1,119 @@
+﻿using System.Collections;
+using System.Dynamic;
+using System.Runtime.Intrinsics.Arm;
+
+public class EjercicioPublicidades {
+    static void Main() {
+
+        //inicializo 3 medios
+        IMedio Viva = new Revista();       
+        IMedio Clarin = new Diario();
+        IMedio Infobae = new Folleto();
+        
+        //inicializo una publicidad con un medio (de clase diario)
+        Publicidad publi01 = new(8.00, 750, Clarin);
+        //esperado: costo 800
+        Console.WriteLine("El costo para publicar esta publicidad en este diario es de "+publi01.CostoPublicacion());
+        
+        //cambio el medio  de la publicidad (con uno de clase revista)
+        publi01.SetMedio(Viva);
+        //esperado: costo 450
+        Console.WriteLine("El costo para publicar esta publicidad en esta revista es de "+publi01.CostoPublicacion());
+        
+        //cambio a otro (de clase folleto)
+        publi01.SetMedio(Infobae);
+        //esperado: costo 550
+        Console.WriteLine("El costo para publicar esta publicidad en este folleto es de "+publi01.CostoPublicacion());
+        
+
+    }
+    public class Publicidad {
+        private double centimetros;
+        private int cantCaracteres;
+        
+        private IMedio medio;
+
+        public Publicidad(double cm, int cc, IMedio m)
+        {
+            centimetros = cm;
+            cantCaracteres = cc;
+            medio = m;
+            SetConditionOfMedio();
+        }
+
+        public void SetMedio(IMedio m) {
+            medio = m;
+            SetConditionOfMedio();
+        }
+        /*depentiendo el tipo de medio este método llama al mismo método pero con diferentes
+        parámetros (overload) */
+        public void SetConditionOfMedio(){
+            if (GetMedio() is Revista){
+                medio.SetCondition(GetCM());
+            }
+            if (GetMedio() is Diario) {
+                medio.SetCondition(GetCantCar());
+            }
+            if (medio is Folleto) {
+                medio.SetCondition();
+            }
+        }      
+
+        public double GetCM(){
+            return centimetros;
+        }
+        public int GetCantCar(){
+            return cantCaracteres;
+        }
+        public IMedio GetMedio(){
+            return medio;
+        }
+
+        public double CostoPublicacion() {
+            return 50+medio.CostoTotal();
+        }
+    }
+    public interface IMedio {
+
+        /* por el overload, la interfase selecciona uno de los tres métodos*/
+        public void SetCondition(){
+        }
+        public void SetCondition(double cm){
+        }
+        public void SetCondition(int cantCar){
+        }
+        public double CostoTotal(){
+            return 0.00;
+        }
+    }
+    
+    public class Revista : IMedio {
+        private double centimetros;
+        /*cada clase que usa la interfase overridea solamente uno de los 3 métodos
+        que fue preseleccionado en el método SetConditionOfMedio*/
+        public void SetCondition(double cm){
+            centimetros = cm;
+        }
+        public double CostoTotal(){
+            return 50*centimetros;
+        }
+    }
+    public class Diario : IMedio {
+        private int cantCaracteres;
+        public void SetCondition(int cc){
+            cantCaracteres = cc;
+        }
+        public double CostoTotal(){
+            return cantCaracteres;
+        }
+    }
+
+    public class Folleto : IMedio {
+        public double CostoTotal(){
+            return 500;
+        }
+    }
+
+
+
+}
